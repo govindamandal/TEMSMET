c25ffa8bdcdedc04a0f9b41590b92ff53abc8e6b
Govinda Mandal <govinda4india@gmail.com>
2024-09-17 22:30:54 +0530

    Added shutter images for delhi

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
-function tab(id) {
-    const page = document.getElementById(id)   
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const aboutLink = document.getElementById("about-link");
+    const aboutSubmenu = document.getElementById("about-submenu");
+    console.log('aboutLink ', aboutLink);
+    
+    // Toggle submenu visibility on click
+    aboutLink.addEventListener("click", function (e) {
+        e.preventDefault(); // Prevent default link behavior
+        if (aboutSubmenu.classList.contains("submenu-visible")) {
+            aboutSubmenu.classList.remove("submenu-visible");
+            aboutSubmenu.classList.add("submenu-hidden");
+        } else {
+            aboutSubmenu.classList.remove("submenu-hidden");
+            aboutSubmenu.classList.add("submenu-visible");
+        }
+    });
 
-(() => {
-    const menus = document.querySelectorAll('menu a')
-    menus.forEach((a) => {
-        
-    })
-})()
+    // Close the submenu if clicked outside
+    document.addEventListener("click", function (e) {
+        if (!aboutLink.contains(e.target) && !aboutSubmenu.contains(e.target)) {
+            aboutSubmenu.classList.remove("submenu-visible");
+            aboutSubmenu.classList.add("submenu-hidden");
+        }
+    });
+});
